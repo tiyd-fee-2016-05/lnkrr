b2bca1f938b83bbb0b292b0be2cb40cc9f5a2143
Jordan Long <jordan.long222@gmail.com>
2016-06-09 15:51:52 -0400

    add test json object array w/ for loop removing each title

--- assets/js/lnkrr.js
+++ assets/js/lnkrr.js
@@ -2,11 +2,25 @@ $(function(){
    'use strict';
 
   $('.userProfile').click(function () {
-console.log('1');
    $.getJSON('apis/lnkrr/users/skywalker/skywalker.json', function(json) {
-console.log('2');
-     $(".userProfile").append(json.username);
+     $(".userProfile").append('<li id="avatar">' + '<img src="' + json.avatar + '" />' + '</li>',
+                              '<li id="username">' + json.username + '</li>',
+                              '<li id="fullname">' + json.first_name + ' ' + json.last_name + '</li>',
+                              '<li id="location">' + json.location + '</li>',
+                              '<li id="joined_date">' + json.joined_date + '</li>',
+                              '<li id="saved_links">' + json.saved_links + '</li>'
+                              )
 
    });
   });
+
+  $('.savedLinks').click(function () {
+    console.log('hey');
+    $.getJSON('apis/lnkrr/users/skywalker/links.json', function(json) {
+      for (var l = 0; l < json.length; l++){
+        $(".savedLinks").append('<li class="linkList">' + json[l].title + '</li>');
+      }
+    });
+  });
+
 });
