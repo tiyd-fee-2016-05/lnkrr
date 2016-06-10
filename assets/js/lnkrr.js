cf3a4504d43420ff1277e306135d7342cd8aec49
Jordan Long <jordan.long222@gmail.com>
2016-06-10 12:22:32 -0400

    add authorization and ajax for saved links. add some styling to main content

--- assets/js/lnkrr.js
+++ assets/js/lnkrr.js
@@ -9,8 +9,17 @@ $(function(){
 
   searchUser = $('input[name="searchUser"]').val();
 
-   $.getJSON('apis/lnkrr/users/' + searchUser + '/' + searchUser + '.json', function(json) {
-     $(".userProfile").append('<li id="avatar">' + '<img src="' + json.avatar + '" />' + '</li>',
+  $.ajax({
+  dataType: 'json',
+  url: 'http://lnkrr.herokuapp.com/' + searchUser,
+  method: 'GET',
+  beforeSend: function (xhr) {
+    var base64Credentials = btoa(searchUser + ":" + 'cool123');
+    xhr.setRequestHeader('Authorization', 'Basic ' + base64Credentials);
+    }
+  }).done(function (json) {
+    console.log(json);
+    $(".userProfile").append('<li id="avatar">' + '<img src="' + json.avatar + '" />' + '</li>',
                               '<li id="username">' + json.username + '</li>',
                               '<li id="fullname">' + json.first_name + ' ' + json.last_name + '</li>',
                               '<li id="location">' + json.location + '</li>',
@@ -18,16 +27,32 @@ $(function(){
                               '<li id="saved_links">' + json.saved_links + '</li>'
                             );
 
-   });
   });
 
-  $('.searchbar').submit(function () {
-    console.log('hey');
-    $.getJSON('apis/lnkrr/users/' + searchUser + '/links.json', function(json) {
-      for (var l = 0; l < json.length; l++){
-        $(".savedLinks").append('<li class="linkList">' + json[l].title + '</li>');
+  // $('.searchbar').submit(function () {
+  //   console.log('hey');
+  //   $.getJSON('apis/lnkrr/users/' + searchUser + '/links.json', function(json) {
+  //     for (var l = 0; l < json.length; l++){
+  //       $(".savedLinks").append('<li class="linkList">' + json[l].title + '</li>');
+  //     }
+  //   });
+  // });
+
+
+  $.ajax({
+    dataType: 'json',
+    url: 'http://lnkrr.herokuapp.com/' + searchUser + '/links',
+    method: 'GET',
+    beforeSend: function (xhr) {
+      var base64Credentials = btoa(searchUser + ":" + 'cool123');
+      xhr.setRequestHeader('Authorization', 'Basic ' + base64Credentials);
       }
-    });
+  }).done(function(json) {
+    console.log(json);
+    for (var l = 0; l < json.length; l++){
+      $(".savedLinks").append('<li class="linkList">' + json[l].title + '</li>');
+    }
   });
 
+});
 });
